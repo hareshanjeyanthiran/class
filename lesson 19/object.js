1a7b9d60be1e18a3f9cb544819bb5846293de0f2
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-04-12 19:08:31 +0400

    Update

---
+++ lesson 19/object.js
@@ -0,0 +1,27 @@
+// Object = collection of data in a key-value manner
+
+const student = {
+    Name:"Hareshan",
+    Age:12,
+    Class: 7,
+    School:"AGS"
+}
+const student2 ={
+    Name:"Ram",
+    Age : 18,
+    wish : function(){
+        console.log("Happy new year")
+    }
+}
+
+const Person ={
+    person1: {
+        Name:"Kartik"
+    },
+    person2: {
+        Name: "Robux"
+
+    }
+}
+
+console.log(student2.wish())
