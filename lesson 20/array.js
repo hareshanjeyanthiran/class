0b6d22689dee98e7ccb3e77716dbe949335cf5ff
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-04-13 19:56:13 +0400

    Update

---
+++ lesson 20/array.js
@@ -0,0 +1,22 @@
+// Index always starts with 0 
+var fruits = ["mango","apple","orange","grapes","pears"]
+//How to extract element
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+//How to find length = total number of elments inside array
+console.log(fruits.length)
+
+//how to add elements
+// fruits.push("banana")
+// fruits.push("lemon")
+// console.log(fruits);
+
+//How to remove elements
+fruits.pop([2])
+fruits.pop([3])
+console.log(fruits)
