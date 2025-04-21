d8052703bd6c1a23f82aba97bdca05705a5efc76
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-04-21 17:13:36 +0400

    Update

---
+++ lesson 23/list.js
@@ -0,0 +1,49 @@
+//list
+
+// var fruits = ["Apple","Guava","Banana","Avacado","Mango"]
+
+//Accesing element
+// console.log(fruits[2])
+
+//Slice
+// console.log(fruits.slice(0,3))
+
+//add
+// fruits.push("Papaya")
+// console.log(fruits)
+
+//remove
+// fruits.pop()
+// fruits.pop()
+// console.log(fruits)
+
+//length
+// console.log(fruits.length);
+
+//for each
+
+// fruits.forEach((i)=>{
+//     console.log(i);
+// })
+
+
+
+
+var games = ["Roblox","Fortknite","Krunker","Minecraft","Arena"]
+
+// console.log(games[2])
+
+
+// console.log(games.slice(0,3))
+
+// games.push("Subway")
+// console.log(games)
+
+// games.pop()
+// games.pop()
+// console.log(games)
+
+
+games.forEach((i)=>{
+    console.log(i);
+})
