04bd11b4b99644d7cabbd354dde200872a33073b
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-04-27 19:55:05 +0400

    Update

---
+++ lesson 25/Arrow.js
@@ -0,0 +1,52 @@
+function wish(name,name2) {
+     console.log("Hello "+name)
+     console.log("Hello "+name2)
+}
+
+wish("Kartik","Gab")
+
+function add(a,b){
+    console.log(a+b);
+}
+
+add(2,3)
+add(5,-7)
+
+function Add(a,b){
+    console.log(a+b)
+}
+
+Add(1,1)
+
+var Add = (a,b) => a+b;
+console.log(Add(7,3))
+
+
+
+function Sub(a,b){
+    console.log(a-b)
+}
+
+Sub(1,1)
+
+var Sub = (a,b) => a-b;
+console.log(Sub(7,3))
+
+
+function Mul(a,b){
+    console.log(a*b)
+}
+
+Mul(16,17)
+
+var Mul = (a,b) => a*b;
+console.log(Mul(7,3))
+
+function Split(a,b){
+    console.log(a/b)
+}
+
+Split(150000,5)
+
+var Split = (a,b) => a*b;
+console.log(Split(7,3))
