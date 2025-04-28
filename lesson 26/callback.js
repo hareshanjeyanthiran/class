631d66d3b40f503bc6f7cc8bdb71fc4e3ae43a6e
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-04-28 17:08:30 +0400

    Update

---
+++ lesson 26/callback.js
@@ -0,0 +1,26 @@
+function Hello(){
+    return "Hello";
+}
+
+function Dear(){
+    return "Dear";
+}
+
+function Wish(name){
+    return Hello()+" "+Dear()+" "+name
+}
+
+console.log(Wish("Hareshan"))
+
+
+
+// function a(){
+//     return "This is A function"
+// }
+
+// function b(){
+//     return a()
+// }
+
+
+// console.log(b());
